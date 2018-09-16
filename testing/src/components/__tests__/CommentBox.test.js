733932222756fb708920c65c513d3771c73d7060
Tanawit Pattanaveerangkoon <tanawit.pattana@outlook.com>
2018-09-16 15:15:35 +0700

    Section 2, Lecture 61

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -17,9 +17,9 @@ afterEach(() => {
     component.unmount()
 })
 
-it('has a text area and a button', () => {
+it('has a text area and two buttons', () => {
     expect(component.find('textarea').length).toEqual(1)
-    expect(component.find('button').length).toEqual(1)
+    expect(component.find('button').length).toEqual(2)
 })
 
 describe('the text area', () => {
