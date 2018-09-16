b114de806521dd5fe7706457e8a4b27830f92826
Tanawit Pattanaveerangkoon <tanawit.pattana@outlook.com>
2018-09-16 21:42:56 +0700

    Section 3, Lecture 72

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {Route, Link} from 'react-router-dom'
 import CommentBox from 'components/CommentBox'
 import CommentList from 'components/CommentList'
+import { connect } from 'react-redux'
+import * as actions from 'actions'
 
-export default () => {
-    return(
-        <div>
-            <CommentBox/>
-            <CommentList/>
-        </div>
-    )
+class App extends Component{
+    renderButton(){
+        if(this.props.auth){
+            return(
+                <button onClick={() => this.props.changeAuth(false)}>Sign out</button>
+            )
+        }else{
+            return(
+                <button onClick={() => this.props.changeAuth(true)}>Sign in</button>
+            )
+        }
+    }
+    renderHeader(){
+        return(
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/post'>Post a comment</Link></li>
+                <li>{this.renderButton()}</li>
+            </ul>
+        )
+    }
+    render(){
+        return(
+            <div>
+                {this.renderHeader()}
+                <Route path='/post' component={CommentBox}/>
+                <Route exact path='/' component={CommentList}/>
+            </div>
+        )    
+    }
 }
+
+function mapStateToProps(state){
+    return{auth: state.auth}
+}
+
+export default connect(mapStateToProps, actions)(App)
